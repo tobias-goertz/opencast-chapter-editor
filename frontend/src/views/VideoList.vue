6fba12d3cfa4d21c6010e7bca1ffbe03fce54f6f
Tobias Görtz <tobias.goertz@zweitag.de>
2020-08-22 14:15:42 +0200

    add Video-list component for dev reasons

---
+++ frontend/src/views/VideoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-container>
+    <h1>Videos</h1>
+    <hr><br><br>
+    <ul>
+      <li v-for="(video, index) in videos" :key="index">
+        <router-link :to="{ name: 'editor-index', params: { id: video.id }}">
+          {{ video.title }}
+        </router-link>
+      </li>
+    </ul>
+  </b-container>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      videos: [],
+      location: process.env.VUE_APP_BACKEND_PROXY_PASS_LOCATION || '',
+      url: process.env.VUE_APP_BACKEND_URL || '',
+    };
+  },
+  methods: {
+    getVideos() {
+      const path = `${this.url}${this.location}/videos`;
+      axios.get(path)
+        .then((res) => {
+          this.videos = res.data.results;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+  created() {
+    this.getVideos();
+  },
+};
+</script>
