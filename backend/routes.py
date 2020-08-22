88e137ec8d0091b2024daaf3486eb9183c3aaeb4
Tobias Görtz <tobias.goertz@zweitag.de>
2020-08-22 13:57:03 +0200

    add segments route

--- backend/routes.py
+++ backend/routes.py
@@ -1,2 +1,24 @@
 from flask import request, make_response, jsonify
 from backend import app, session
+import json
+import xmltodict
+from natsort import natsorted
+from os import environ
+from datetime import datetime
+
+opencast_url = environ.get('OPENCAST_URL')
+
+
+@app.route('/segments')
+def segments():
+    id = request.args.get('id')
+    if id:
+        r = session.get(
+            f'{opencast_url}/search/episode.json?id={id}',
+            auth=('admin', 'opencast')).json()
+        if 'segments' in r['search-results']['result']:
+            return r['search-results']['result']['segments']
+        else:
+            return error("No Segments found", 404)
+    else:
+        return error("No ID provided", 422)
