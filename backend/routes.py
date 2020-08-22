204c70ac4d80495067a8b024352b7047116d82f2
Tobias Görtz <tobias.goertz@zweitag.de>
2020-08-22 13:58:24 +0200

    get Videos for multiple sources and sort them

--- backend/routes.py
+++ backend/routes.py
@@ -31,6 +31,7 @@ def videos():
                auth=('admin', 'opencast')).json()
     return res
 
+
 @app.route('/video')
 def video():
     id = request.args.get('id')
@@ -40,6 +41,55 @@ def video():
                    auth=('admin', 'opencast')).json()
         if res:
             return res
+        else:
+            return error("Video not found", 404)
+    else:
+        return error("No ID provided", 422)
+
+
+@app.route('/search')
+def search():
+    id = request.args.get('id')
+    if id:
+        req = session.get(
+                   f'{opencast_url}/search/episode.json?id={id}',
+                   auth=('admin', 'opencast')).json()
+        if 'result' in req['search-results']:
+            mediapackage = req['search-results']['result']['mediapackage']
+            tracks = mediapackage['media']['track']
+            v = {}
+            presenter = []
+            presentation = []
+            other = []
+
+            if type(tracks) is dict:
+                tracks = [tracks]
+
+            for track in tracks:
+                td = {}
+                td['id'] = track['id']
+                td['type'] = track['type']
+                td['url'] = track['url']
+                td['duration'] = track['duration'] / 1000
+                td['resolution'] = track['video']['resolution']
+                if 'presenter' in track['type']:
+                    presenter.append(td)
+                elif 'presentation' in track['type']:
+                    presentation.append(td)
+                else:
+                    other.append(td)
+
+            v['presenter'] = natsorted(
+                                        presenter,
+                                        key=lambda x: x['resolution']
+                                      )
+            v['presentation'] = natsorted(
+                                        presentation,
+                                        key=lambda x: x['resolution']
+                                      )
+            v['other'] = natsorted(other, key=lambda x: x['resolution'])
+            v['title'] = mediapackage['title']
+            return v
         else:
             return error("Video not found", 404)
     else:
