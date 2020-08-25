51238a3d62ee9072c23f64d63b1fb85ef116f7ba
Tobias Görtz <tobias.goertz@zweitag.de>
2020-08-25 12:24:25 +0200

    use only first code from de-DE for locale

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -30,7 +30,7 @@ Vue.use(FlashMessage, {
 
 // eslint-disable-next-line
 const i18n = new VueI18n({
-  locale: navigator.language || process.env.VUE_APP_I18N_LOCALE,
+  locale: navigator.language.split('-')[0] || process.env.VUE_APP_I18N_LOCALE,
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages,
 });
