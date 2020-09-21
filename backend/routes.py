3126dcd674b2be1be4b3601aabbbf546f9f28b12
Tobias Görtz <tobias.goertz@zweitag.de>
2020-09-21 10:02:05 +0200

    refactoring: router to services

--- backend/routes.py
+++ backend/routes.py
@@ -1,12 +1,9 @@
 from flask import request
-from backend import app, session, opencast_url
-import json
-import xmltodict
-from natsort import natsorted
-from os import environ
+from backend import app
 from .service.settings_service import get_settings
 from .service.segments_service import get_segments, publish_segments
 from .service.response_service import error
+from .service.video_service import get_video, get_video_list, get_public_event
 
 
 # returns settings from oc-config via settings_service
@@ -28,10 +25,7 @@ def segments():
 # video index for dev and testing only
 @app.route('/videos')
 def videos():
-    res = session.get(
-               f'{opencast_url}/api/events').json()
-    payload = dict(videos=res, opencastUrl=opencast_url)
-    return json.dumps(payload)
+    return get_video_list()
 
 
 # returns event including publication (not used anymore)
@@ -39,13 +33,8 @@ def videos():
 def video():
     id = request.args.get('id')
     if id:
-        res = session.get(
-                   f'{opencast_url}/api/events/{id}?withpublications=true',
-              ).json()
-        if res:
-            return res
-        else:
-            return error("Video not found", 404)
+        result = get_public_event(id)
+        return result
     else:
         return error("No ID provided", 400)
 
@@ -55,49 +44,8 @@ def video():
 def search():
     id = request.args.get('id')
     if id:
-        req = session.get(
-                   f'{opencast_url}/search/episode.json?id={id}').json()
-        if 'result' in req['search-results']:
-            mediapackage = req['search-results']['result']['mediapackage']
-            tracks = mediapackage['media']['track']
-            presenter = []
-            presentation = []
-            other = []
-
-            if type(tracks) is dict:
-                tracks = [tracks]
-
-            for track in tracks:
-                td = dict()
-                td['id'] = track.get('id')
-                td['type'] = track.get('type')
-                td['url'] = track.get('url')
-                td['duration'] = track.get('duration') / 1000
-                td['resolution'] = track['video']['resolution']
-                if 'presenter' in track['type']:
-                    presenter.append(td)
-                elif 'presentation' in track['type']:
-                    presentation.append(td)
-                else:
-                    other.append(td)
-
-            presenter = natsorted(
-                                   presenter,
-                                   key=lambda x: x.get('resolution')
-                                 )
-            presentation = natsorted(
-                                      presentation,
-                                      key=lambda x: x.get('resolution')
-                                    )
-            other = natsorted(other, key=lambda x: x.get('resolution'))
-            title = mediapackage.get('title')
-            v = dict(presenter=presenter,
-                     presentation=presentation,
-                     other=other,
-                     title=title)
-            return v
-        else:
-            return error("Video not found", 404)
+        result = get_video(id)
+        return result.get('message')
     else:
         return error("No ID provided", 422)
 
---
+++ backend/service/video_service.py
@@ -0,0 +1,67 @@
+from backend import session, opencast_url
+from .response_service import error, success
+from natsort import natsorted
+import json
+
+
+def get_video(id):
+    req = session.get(
+               f'{opencast_url}/search/episode.json?id={id}').json()
+    if 'result' in req['search-results']:
+        mediapackage = req['search-results']['result']['mediapackage']
+        tracks = mediapackage['media']['track']
+        presenter = []
+        presentation = []
+        other = []
+
+        if type(tracks) is dict:
+            tracks = [tracks]
+
+        for track in tracks:
+            td = dict()
+            td['id'] = track.get('id')
+            td['type'] = track.get('type')
+            td['url'] = track.get('url')
+            td['duration'] = track.get('duration') / 1000
+            td['resolution'] = track['video']['resolution']
+            if 'presenter' in track['type']:
+                presenter.append(td)
+            elif 'presentation' in track['type']:
+                presentation.append(td)
+            else:
+                other.append(td)
+
+        presenter = natsorted(
+                               presenter,
+                               key=lambda x: x.get('resolution')
+                             )
+        presentation = natsorted(
+                                  presentation,
+                                  key=lambda x: x.get('resolution')
+                                )
+        other = natsorted(other, key=lambda x: x.get('resolution'))
+        title = mediapackage.get('title')
+        v = dict(presenter=presenter,
+                 presentation=presentation,
+                 other=other,
+                 title=title)
+        return success(v, 200)
+    else:
+        return error("Video not found", 404)
+
+
+def get_video_list():
+    res = session.get(
+               f'{opencast_url}/api/events').json()
+    payload = dict(videos=res, opencastUrl=opencast_url)
+    return json.dumps(payload)
+
+
+def get_public_event(id):
+    res = session.get(
+               f'{opencast_url}/api/events/{id}?withpublications=true',
+          ).json()
+    if res:
+        return success(res, 200)
+    else:
+        return error("Video not found", 404)
