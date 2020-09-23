36d01a5dcc60c4ab808ee50ceec621ee3ba2f5d4
Tobias Görtz <tobias.goertz@zweitag.de>
2020-09-23 12:33:45 +0200

    read settings from oc

--- backend/service/settings_service.py
+++ backend/service/settings_service.py
@@ -1,15 +1,16 @@
 import json
+from backend import session, opencast_url
+from .response_service import error
 
 
 def get_settings():
-    # deactivated until /chapter-editor/settings.json is available
-    # res = session.get(
-    #       f'{opencast_url}/ui/config/studio/settings.json',
-    #       auth=opencast_auth)
-    # if res.status_code != 200:
-    #     return error(res.text, res.status_code)
-    # else:
-    #     return res.json()
-    with open('ui-settings-sample.json') as json_file:
-        data = json.load(json_file)
-    return data
+    # in case /chapter-editor/settings.json is not available:
+    # with open('ui-settings-sample.json') as json_file:
+    #     data = json.load(json_file)
+    # return data
+    res = session.get(
+          f'{opencast_url}/ui/config/chapter-editor/settings.json')
+    if res.status_code != 200:
+        return error(res.text, res.status_code)
+    else:
+        return res.json()
